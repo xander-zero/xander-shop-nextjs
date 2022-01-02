f46c3e570de6c35650368502e0a44ec7613b605c
xander-zero <mahmud.ansari60636@gmail.com>
2022-01-02 21:09:09 +0330

    creating list of products

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,47 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import data from "../utils/data";
 
 export default function Home() {
+  const { products } = data;
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Produc 1</li>
-          <li>Produc 2</li>
-          <li>Produc 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  />
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                    <Typography>${product.price}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Add To Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
