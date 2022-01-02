5d3cb3f7b9efad84ae8520c91edb42cbb0f16632
xander-zero <mahmud.ansari60636@gmail.com>
2022-01-02 19:04:35 +0330

    add style to navbar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,19 +1,23 @@
 import Head from "next/head";
 import React from "react";
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+// useStyles
+import useStyles from "../../utils/styles";
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Xander Shop</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <Typography>Xander Shop</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved Xander Shop</Typography>
       </footer>
     </div>
